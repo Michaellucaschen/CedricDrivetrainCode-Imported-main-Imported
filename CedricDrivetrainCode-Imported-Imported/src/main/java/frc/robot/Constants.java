05d7b43da5e19e5c1e3c1ee60f4394779cc423c5
Michael Chen <michael.lucas.chen@gmail.com>
2025-01-11 14:46:40 -0600

    add arm

--- CedricDrivetrainCode-Imported-Imported/src/main/java/frc/robot/Constants.java
+++ CedricDrivetrainCode-Imported-Imported/src/main/java/frc/robot/Constants.java
@@ -99,4 +99,9 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
-}
+  public static final int m_ArmSpark = 50;
+
+  
+    
+  
+  }
---
+++ CedricDrivetrainCode-Imported-Imported/src/main/java/frc/robot/subsystems/armsubsystem.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+// import edu.wpi.first.wpilibj2.smartdashboard
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class armsubsystem extends SubsystemBase{
+    private final SparkMax m_ArmSpark;
+
+    /** Creates a new armsubsystem. */
+    public armsubsystem(int m_ArmSpark){
+        
+    }
+}
+
+}
